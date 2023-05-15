6ea87d3725cac656f1836a0ce216efa54f1ca2af
Frank Jogeleit <frank.jogeleit@lovoo.com>
2023-05-15 17:08:03 +0200

    feat: onboarding
    Signed-off-by: Frank Jogeleit <frank.jogeleit@lovoo.com>

---
+++ frontend/src/components/OnboardingButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-btn :color="btnColor" variant="outlined">Onboarding</v-btn>
+</template>
+
+<script setup lang="ts">
+import { btnColor } from '@/config';
+
+</script>
---
+++ frontend/src/composables/onboarding.ts
@@ -0,0 +1,52 @@
+import { useVOnboarding } from "v-onboarding"
+import { Ref, ref } from "vue"
+
+export const useOnboarding = (drawer: Ref<boolean>) => {
+    const wrapper = ref(null)
+    const onboarding = ref(true)
+    const { start, finish } = useVOnboarding(wrapper)
+
+    const steps = [
+        {
+            attachTo: { element: '#example-menu' },
+            content: { title: "Example Menu", description: "Open a list of predefined Policies." }
+        },
+        {
+            attachTo: { element: '#tutorials' },
+            content: { title: "Examples", description: "Choose between different Categories" },
+            on: {
+                beforeStep: async () => {
+                    drawer.value = true;
+                    await new Promise(r => setTimeout(r, 200))
+                },
+                afterStep: () => {
+                    drawer.value = false
+                }
+            }
+        },
+        {
+            attachTo: { element: '#policy-panel' },
+            content: { title: "Policy Panel", description: "Write and modify your Kyverno Policy." }
+        },
+        {
+            attachTo: { element: '#context-panel' },
+            content: { title: "Context Panel", description: "Define or change your Conext." }
+        },
+        {
+            attachTo: { element: '#resource-panel' },
+            content: { title: "Resource Panel", description: "Write and modify resources which your policy is applied to." }
+        },
+        {
+            attachTo: { element: '#start-btn' },
+            content: { title: "Start Button", description: "Runs your actual Policy against the Resources with the given Context." }
+        },
+    ]
+
+    return {
+        finish,
+        wrapper,
+        start,
+        onboarding,
+        steps
+    }
+}
