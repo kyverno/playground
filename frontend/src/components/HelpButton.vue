082b197effd6f5c131114f4713822977989f06d4
Frank Jogeleit <frank.jogeleit@web.de>
2023-05-16 16:20:31 +0200

    feat: improve helper text (#162)
    Signed-off-by: Frank Jogeleit <frank.jogeleit@lovoo.com>

--- frontend/src/components/HelpButton.vue
+++ frontend/src/components/HelpButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-dialog v-model="dialog" width="60%" :theme="layoutTheme">
+  <v-dialog v-model="dialog" width="80%" :theme="layoutTheme">
     <template v-slot:activator="{ props }">
       <v-btn
         :height="44"
@@ -19,6 +19,12 @@
         </template>
       </v-toolbar>
       <v-card-text>
+        <h2 class="text-h6 mb-2 mt-3">Project Status</h2>
+
+        <p>
+          Please note that this project is currently in an alpha phase and is released as a preview build. If you encounter any problems, please feel free to file a bug report.
+        </p>        
+
         <h2 class="text-h6 mb-2 mt-3">This Playground supports the following kinds of rules</h2>
 
         <ul class="pl-4 my-2">
@@ -50,6 +56,20 @@
           <li>The "File" Button loads a local YAML file as input</li>
           <li>The "URL" Button loads a manifest from an external URL<br />Example: <a href="https://raw.githubusercontent.com/kyverno/policies/main/best-practices/disallow-latest-tag/disallow-latest-tag.yaml" target="_blank">https://raw.githubusercontent.com/kyverno/policies/main/best-practices/disallow-latest-tag/disallow-latest-tag.yaml</a></li>
         </ul>
+
+        <h2 class="text-h6 mb-2 mt-3">Load / Save Functionality</h2>
+
+        <ul class="pl-4 my-2 text-body-1">
+          <li>Save new state: Your current state is saved in your local storage as a named profile, which means it is only available on your current machine and browser.</li>
+          <li>Load a profile from your local storage, load the default profile to reset your current state to its defaults.</li>
+          <li>Update a profile by selecting your current profile from the storage menu.</li>
+        </ul>
+
+        <h2 class="text-h6 mb-2 mt-3">Share</h2>
+
+        <p>
+          Share your current state with others by sending the created link. This link contains your state as a compressed base64 string, which means that this link will never expire or become invalid.
+        </p>
       </v-card-text>
       <v-divider />
       <v-card-actions>
