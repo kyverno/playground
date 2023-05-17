49a23a8047c84c93d14e870d25f0ee2a090c83bd
Frank Jogeleit <frank.jogeleit@web.de>
2023-05-17 14:26:27 +0200

    feat: show app version (#189)
    Signed-off-by: Frank Jogeleit <frank.jogeleit@lovoo.com>

---
+++ frontend/src/components/AppVersion.vue
@@ -0,0 +1,25 @@
+<template>
+<v-card class="position background" flat :theme="layoutTheme" v-if="version">
+    <v-card-text class="text-body-2 font-weight-bold pa-2 text-center px-3 py-1" style="font-size: 0.65rem!important">
+        AppVersion: {{ version }}
+    </v-card-text>
+</v-card>
+</template>
+
+<script setup lang="ts">
+import { layoutTheme } from '@/config';
+
+const version = APP_VERSION
+</script>
+
+<style scoped>
+.background {
+    background-color: rgb(var(--v-theme-background))
+}
+
+.position {
+  position: fixed;
+  right: 0;
+  bottom: 0px;
+}
+</style>
