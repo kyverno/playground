fcdd0d7688e146bb0fa6c7fcdf280d37ef4db415
Frank Jogeleit <frank.jogeleit@web.de>
2023-05-17 15:19:13 +0200

    feat: load Policy from kyverno/policies repo via query parameter (#190)
    Signed-off-by: Frank Jogeleit <frank.jogeleit@lovoo.com>

--- frontend/src/functions/github.ts
+++ frontend/src/functions/github.ts
@@ -1,8 +1,12 @@
-import { ContextTemplate } from "@/assets/templates";
+import { ContextTemplate, ResourceTemplate } from "@/assets/templates";
+import { init } from "@/store";
+
+const baseURL = 'https://raw.githubusercontent.com/kyverno/policies/main'
 
 export type Policy = {
     url?: string;
     contextPath?: string;
+    resourceFile?: string;
     path: string;
     title: string;
 }
@@ -21,16 +25,23 @@ export const loadPolicy = async (url: string, policy: Policy, config?: LoadConfi
 
     try {
         const contextURL = contextPath ? `${contextPath}/${name}.yaml` : `${url}/${folder}/context.yaml`
+        const resourceFile = policy.resourceFile || 'resource.yaml'
 
         const promises = [
             fetch(`${url}/${folder}/${name}.yaml`).then((resp) => resp.text()),
-            fetch(`${url}/${folder}/resource.yaml`).then((resp) => {
+            fetch(`${url}/${folder}/${resourceFile}`).then((resp) => {
                 if (resp.status === 404) {
                     return fetch(`${url}/${folder}/resources.yaml`)
                 }
 
                 return resp
-            }).then((resp) => resp.text()),
+            }).then((resp) => {
+                if (resp.status === 404) {
+                    return ResourceTemplate
+                }
+
+                return resp.text()
+            }),
             fetch(contextURL).then((resp) => {
                 if (resp.status === 404) {
                     return ContextTemplate
@@ -50,3 +61,19 @@ export const loadPolicy = async (url: string, policy: Policy, config?: LoadConfi
         config?.finished?.()
     }
 };
+
+export const loadFromRepo = (path: string, resource?: string, config?: LoadConfig) => {
+    return loadPolicy(baseURL, {
+        path: path.replace(/^\/+/, '').replace(/\/+$/, '').trim(),
+        resourceFile: resource,
+        title: ''
+    }, {
+        success([policy, resource, context]) {
+            init({ policy, resource, context })
+        },
+        error(err) {
+            console.error(err)
+        },
+        ...(config || {}),
+    })
+}
