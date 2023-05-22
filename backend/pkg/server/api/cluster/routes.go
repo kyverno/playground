b8ed33080753d81594ec16e8779df7b491afa8f4
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-05-22 15:27:49 +0200

    fix: use GET to fetch resource
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- backend/pkg/server/api/cluster/routes.go
+++ backend/pkg/server/api/cluster/routes.go
@@ -17,10 +17,10 @@ type SearchRequest struct {
 type SearchResponse = []SearchResult
 
 type ResourceRequest struct {
-	APIVersion string `json:"apiVersion"`
-	Kind       string `json:"kind"`
-	Namespace  string `json:"namespace"`
-	Name       string `json:"name"`
+	APIVersion string `query:"apiVersion"`
+	Kind       string `query:"kind"`
+	Namespace  string `query:"namespace"`
+	Name       string `query:"name"`
 }
 
 func AddRoutes(group *gin.RouterGroup, cluster Cluster) error {
@@ -43,16 +43,10 @@ func AddRoutes(group *gin.RouterGroup, cluster Cluster) error {
 	group.GET("/search", tonic.Handler(func(c *gin.Context, in *SearchRequest) (SearchResponse, error) {
 		return cluster.Search(c, in.APIVersion, in.Kind, in.Namespace, in.Selector)
 	}, http.StatusOK))
-	group.POST("/resource", func(c *gin.Context) {
-		var request ResourceRequest
-		if err := c.ShouldBind(&request); err != nil {
-			c.String(http.StatusBadRequest, err.Error())
-			return
-		}
-		resource, err := cluster.Get(c, request.APIVersion, request.Kind, request.Namespace, request.Name)
+	group.GET("/resource", tonic.Handler(func(c *gin.Context, in *ResourceRequest) (map[string]interface{}, error) {
+		resource, err := cluster.Get(c, in.APIVersion, in.Kind, in.Namespace, in.Name)
 		if err != nil {
-			c.String(http.StatusInternalServerError, "failed to fetch resource")
-			return
+			return nil, err
 		}
 		// cleanup metadata
 		if meta, ok := resource.Object["metadata"]; ok {
@@ -65,7 +59,7 @@ func AddRoutes(group *gin.RouterGroup, cluster Cluster) error {
 		if meta, ok := resource.Object["spec"]; ok {
 			delete(meta.(map[string]any), "strategy")
 		}
-		c.JSON(http.StatusOK, resource.Object)
-	})
+		return resource.Object, err
+	}, http.StatusOK))
 	return nil
 }
