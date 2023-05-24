270fe619836501543d154fa77ed32e3827914f7a
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-05-24 17:53:58 +0200

    feat: use cobra command (#272)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- backend/main.go
+++ backend/main.go
@@ -1,89 +1,15 @@
 package main
 
 import (
-	"context"
-	"flag"
-	"os/signal"
-	"syscall"
-	"time"
+	"fmt"
+	"os"
 
-	"github.com/gin-gonic/gin"
-
-	"github.com/kyverno/playground/backend/pkg/server"
-	"github.com/kyverno/playground/backend/pkg/server/api/cluster"
-	"github.com/kyverno/playground/backend/pkg/utils"
+	"github.com/kyverno/playground/backend/pkg/cmd"
 )
 
-type options struct {
-	host       string
-	port       int
-	mode       string
-	log        bool
-	cors       bool
-	kubeConfig string
-	sponsor    string
-}
-
-func getOptions() options {
-	var options options
-	flag.StringVar(&options.host, "host", "0.0.0.0", "server host")
-	flag.IntVar(&options.port, "port", 8080, "server port")
-	flag.StringVar(&options.mode, "mode", gin.ReleaseMode, "gin run mode")
-	flag.BoolVar(&options.log, "log", false, "enable gin logger")
-	flag.BoolVar(&options.cors, "cors", true, "enable gin cors")
-	flag.StringVar(&options.kubeConfig, "kubeconfig", "", "enable gin logger")
-	flag.StringVar(&options.sponsor, "sponsor", "", "sponsor text")
-	flag.Parse()
-	return options
-}
-
 func main() {
-	// get options from command line parameters
-	options := getOptions()
-	// initialise gin framework
-	gin.SetMode(options.mode)
-	// create server
-	server, err := server.New(options.log, options.cors)
-	if err != nil {
-		panic(err)
-	}
-	// register UI routes
-	if err := server.AddUIRoutes(); err != nil {
-		panic(err)
-	}
-	// register API routes (with/without cluster support)
-	if options.kubeConfig != "" {
-		// create rest config
-		restConfig, err := utils.RestConfig(options.kubeConfig)
-		if err != nil {
-			panic(err)
-		}
-		// create cluster
-		cluster, err := cluster.New(restConfig)
-		if err != nil {
-			panic(err)
-		}
-		// register API routes
-		if err := server.AddAPIRoutes(cluster, options.sponsor); err != nil {
-			panic(err)
-		}
-	} else {
-		// register API routes
-		if err := server.AddAPIRoutes(nil, options.sponsor); err != nil {
-			panic(err)
-		}
-	}
-	// run server
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-	shutdown := server.Run(ctx, options.host, options.port)
-	<-ctx.Done()
-	stop()
-	if shutdown != nil {
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		if err := shutdown(ctx); err != nil {
-			panic(err)
-		}
+	rootCmd := cmd.NewRootCommand()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
 	}
 }
---
+++ backend/pkg/cmd/root.go
@@ -0,0 +1,113 @@
+package cmd
+
+import (
+	"context"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
+	"k8s.io/client-go/tools/clientcmd"
+
+	"github.com/kyverno/playground/backend/pkg/server"
+	"github.com/kyverno/playground/backend/pkg/server/api/cluster"
+	"github.com/kyverno/playground/backend/pkg/utils"
+)
+
+type commandFlags struct {
+	serverFlags  serverFlags
+	ginFlags     ginFlags
+	configFlags  configFlags
+	clusterFlags clusterFlags
+}
+
+type serverFlags struct {
+	host string
+	port int
+}
+
+type ginFlags struct {
+	mode string
+	log  bool
+	cors bool
+}
+
+type configFlags struct {
+	cluster bool
+	sponsor string
+}
+
+type clusterFlags struct {
+	kubeConfigOverrides clientcmd.ConfigOverrides
+}
+
+func NewRootCommand() *cobra.Command {
+	command := commandFlags{}
+	res := &cobra.Command{
+		RunE: command.Run,
+	}
+	// server flags
+	res.Flags().StringVar(&command.serverFlags.host, "server-host", "0.0.0.0", "server host")
+	res.Flags().IntVar(&command.serverFlags.port, "server-port", 8080, "server port")
+	// gin flags
+	res.Flags().StringVar(&command.ginFlags.mode, "gin-mode", gin.ReleaseMode, "gin run mode")
+	res.Flags().BoolVar(&command.ginFlags.log, "gin-log", false, "enable gin logger")
+	res.Flags().BoolVar(&command.ginFlags.cors, "gin-cors", true, "enable gin cors")
+	// config flags
+	res.Flags().StringVar(&command.configFlags.sponsor, "sponsor", "", "sponsor text")
+	res.Flags().BoolVar(&command.configFlags.cluster, "cluster", false, "enable cluster connected mode")
+	// cluster flags
+	clientcmd.BindOverrideFlags(&command.clusterFlags.kubeConfigOverrides, res.Flags(), clientcmd.RecommendedConfigOverrideFlags("kube-"))
+	return res
+}
+
+func (c *commandFlags) Run(_ *cobra.Command, args []string) error {
+	// initialise gin framework
+	gin.SetMode(c.ginFlags.mode)
+	// create server
+	server, err := server.New(c.ginFlags.log, c.ginFlags.cors)
+	if err != nil {
+		return err
+	}
+	// register UI routes
+	if err := server.AddUIRoutes(); err != nil {
+		return err
+	}
+	// register API routes (with/without cluster support)
+	if c.configFlags.cluster {
+		// create rest config
+		restConfig, err := utils.RestConfig(c.clusterFlags.kubeConfigOverrides)
+		if err != nil {
+			return err
+		}
+		// create cluster
+		cluster, err := cluster.New(restConfig)
+		if err != nil {
+			return err
+		}
+		// register API routes
+		if err := server.AddAPIRoutes(cluster, c.configFlags.sponsor); err != nil {
+			return err
+		}
+	} else {
+		// register API routes
+		if err := server.AddAPIRoutes(nil, c.configFlags.sponsor); err != nil {
+			return err
+		}
+	}
+	// run server
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	shutdown := server.Run(ctx, c.serverFlags.host, c.serverFlags.port)
+	<-ctx.Done()
+	stop()
+	if shutdown != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := shutdown(ctx); err != nil {
+			return err
+		}
+	}
+	return nil
+}
