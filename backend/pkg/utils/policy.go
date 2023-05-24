235df036fd7ed00e343327b7b1376ec24e5df7ff
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-05-24 14:35:55 +0200

    refactor: loader code (#266)
    * chore: bump kyverno
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    * refactor: loader code
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>
    
    ---------
    
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

---
+++ backend/pkg/utils/policy.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"fmt"
+
+	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
+	"github.com/kyverno/playground/backend/pkg/resource/convert"
+	"github.com/kyverno/playground/backend/pkg/resource/loader"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+func ToPolicyInterface(untyped unstructured.Unstructured) (kyvernov1.PolicyInterface, error) {
+	kind := untyped.GetKind()
+	if kind == "Policy" {
+		policy, err := convert.To[kyvernov1.Policy](untyped)
+		if err != nil {
+			return nil, err
+		}
+		return &policy, nil
+	} else if kind == "ClusterPolicy" {
+		policy, err := convert.To[kyvernov1.ClusterPolicy](untyped)
+		if err != nil {
+			return nil, err
+		}
+		return &policy, nil
+	}
+	return nil, fmt.Errorf("invalid kind: %s", kind)
+}
+
+func LoadPolicies(l loader.Loader, content []byte) ([]kyvernov1.PolicyInterface, error) {
+	untyped, err := loader.LoadResources(l, content)
+	if err != nil {
+		return nil, err
+	}
+	var policies []kyvernov1.PolicyInterface
+	for _, policy := range untyped {
+		policy, err := ToPolicyInterface(policy)
+		if err != nil {
+			return nil, err
+		}
+		policies = append(policies, policy)
+	}
+	return policies, nil
+}
