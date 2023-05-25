e05a6a14651aa37f25e79afee82f729f9204266b
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-05-25 14:07:38 +0200

    feat: add crds support in request
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- backend/pkg/server/api/engine/request.go
+++ backend/pkg/server/api/engine/request.go
@@ -4,21 +4,25 @@ import (
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/client-go/openapi"
+	"sigs.k8s.io/kubectl-validate/pkg/openapiclient"
 	"sigs.k8s.io/yaml"
 
+	"github.com/kyverno/playground/backend/data"
 	"github.com/kyverno/playground/backend/pkg/engine"
 	"github.com/kyverno/playground/backend/pkg/resource/loader"
 	"github.com/kyverno/playground/backend/pkg/utils"
 )
 
 type EngineRequest struct {
-	Policies  string `json:"policies"`
-	Resources string `json:"resources"`
-	Context   string `json:"context"`
-	Config    string `json:"config"`
+	Policies                  string `json:"policies"`
+	Resources                 string `json:"resources"`
+	Context                   string `json:"context"`
+	Config                    string `json:"config"`
+	CustomResourceDefinitions string `json:"customResourceDefinitions"`
 }
 
-func (r *EngineRequest) ParseContext() (*engine.Parameters, error) {
+func (r *EngineRequest) LoadParameters() (*engine.Parameters, error) {
 	var params engine.Parameters
 	if err := yaml.Unmarshal([]byte(r.Context), &params); err != nil {
 		return nil, err
@@ -39,4 +43,25 @@ func (r *EngineRequest) LoadConfig(resourceLoader loader.Loader) (*corev1.Config
 		return nil, nil
 	}
 	return loader.Load[corev1.ConfigMap](resourceLoader, []byte(r.Config))
+}
+
+func (r *EngineRequest) ResourceLoader(kubeVersion string, config APIConfiguration) (loader.Loader, error) {
+	kubeVersion, err := parseKubeVersion(kubeVersion)
+	if err != nil {
+		return nil, err
+	}
+	clients := []openapi.Client{
+		openapiclient.NewHardcodedBuiltins(kubeVersion),
+	}
+	// TODO: load crds from request
+	// 	if len(r.CustomResourceDefinitions) != 0 {
+	// }
+	for _, crd := range config.LocalCrds {
+		clients = append(clients, openapiclient.NewLocalCRDFiles(nil, crd))
+	}
+	for _, crd := range config.BuiltInCrds {
+		fs, path := data.BuiltInCrds(crd)
+		clients = append(clients, openapiclient.NewLocalCRDFiles(fs, path))
+	}
+	return loader.New(clients...)
 }
