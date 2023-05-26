36e1ef90ee54fd4c7df9795e52caafc33b31788a
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-05-26 09:28:46 +0200

    test: loader test
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- backend/pkg/resource/loader/loader_test.go
+++ backend/pkg/resource/loader/loader_test.go
@@ -2,13 +2,16 @@ package loader
 
 import (
 	"errors"
+	"os"
 	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/client-go/openapi"
 	"sigs.k8s.io/kubectl-validate/pkg/openapiclient"
 	"sigs.k8s.io/kubectl-validate/pkg/validatorfactory"
+	"sigs.k8s.io/yaml"
 
 	"github.com/kyverno/playground/backend/data"
 )
@@ -93,6 +96,87 @@ func TestNew(t *testing.T) {
 			}
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("New() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_loader_Load(t *testing.T) {
+	loadFile := func(path string) []byte {
+		bytes, err := os.ReadFile(path)
+		require.NoError(t, err)
+		return bytes
+	}
+	newLoader := func(client openapi.Client) Loader {
+		loader, err := New(client)
+		require.NoError(t, err)
+		return loader
+	}
+	toUnstructured := func(data []byte) unstructured.Unstructured {
+		json, err := yaml.YAMLToJSON(data)
+		require.NoError(t, err)
+		var result unstructured.Unstructured
+		require.NoError(t, result.UnmarshalJSON(json))
+		if result.GetCreationTimestamp().Time.IsZero() {
+			require.NoError(t, unstructured.SetNestedField(result.UnstructuredContent(), nil, "metadata", "creationTimestamp"))
+		}
+		return result
+	}
+	tests := []struct {
+		name     string
+		loader   Loader
+		document []byte
+		want     unstructured.Unstructured
+		wantErr  bool
+	}{{
+		name:    "nil",
+		loader:  newLoader(openapiclient.NewLocalFiles(data.Schemas(), "schemas")),
+		wantErr: true,
+	}, {
+		name:     "empty GVK",
+		loader:   newLoader(openapiclient.NewLocalFiles(data.Schemas(), "schemas")),
+		document: []byte(`foo: bar`),
+		wantErr:  true,
+	}, {
+		name:   "not yaml",
+		loader: newLoader(openapiclient.NewLocalFiles(data.Schemas(), "schemas")),
+		document: []byte(`
+foo
+  bar
+  - baz`),
+		wantErr: true,
+	}, {
+		name:     "unknown GVK",
+		loader:   newLoader(openapiclient.NewLocalFiles(data.Schemas(), "schemas")),
+		document: loadFile("../../../testdata/namespace.yaml"),
+		wantErr:  true,
+	}, {
+		name:   "bad schema",
+		loader: newLoader(openapiclient.NewHardcodedBuiltins("1.27")),
+		document: []byte(`
+apiVersion: v1
+kind: Namespace
+bad: field
+metadata:
+  name: prod-bus-app1
+  labels:
+    purpose: production`),
+		wantErr: true,
+	}, {
+		name:     "ok",
+		loader:   newLoader(openapiclient.NewHardcodedBuiltins("1.27")),
+		document: loadFile("../../../testdata/namespace.yaml"),
+		want:     toUnstructured(loadFile("../../../testdata/namespace.yaml")),
+	}}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.loader.Load(tt.document)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("loader.Load() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("loader.Load() = %v, want %v", got, tt.want)
 			}
 		})
 	}
