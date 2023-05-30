12e67edfa86cc0304835039dae9b6f09b8841857
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-05-30 10:23:29 +0200

    feat: add auth check when listing kinds (#325)
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- backend/pkg/utils/rest.go
+++ backend/pkg/utils/rest.go
@@ -8,5 +8,11 @@ import (
 func RestConfig(overrides clientcmd.ConfigOverrides) (*rest.Config, error) {
 	loadingRules := clientcmd.NewDefaultClientConfigLoadingRules()
 	kubeConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(loadingRules, &overrides)
-	return kubeConfig.ClientConfig()
+	config, err := kubeConfig.ClientConfig()
+	if err != nil {
+		return nil, err
+	}
+	config.QPS = 300
+	config.Burst = 300
+	return config, nil
 }
