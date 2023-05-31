f38bd41ef417c6121c756c9680241140336701bd
Frank Jogeleit <frank.jogeleit@lovoo.com>
2023-05-31 15:44:28 +0200

    fix: delay suggestions
    Signed-off-by: Frank Jogeleit <frank.jogeleit@lovoo.com>

--- frontend/src/components/Panel/PolicyEditor.vue
+++ frontend/src/components/Panel/PolicyEditor.vue
@@ -20,7 +20,7 @@
 
 <script setup lang="ts">
 import { ref, watch } from 'vue'
-import { editor, Uri, KeyCode } from 'monaco-editor'
+import { editor, KeyCode, Uri } from 'monaco-editor'
 import { editorTheme } from '@/config'
 import { loadedPolicy } from '@/composables'
 import MonacoEditor from './MonacoEditor.vue'
@@ -43,6 +43,8 @@ const options: editor.IStandaloneEditorConstructionOptions = {
   tabSize: 2
 }
 
+let timeout: any = undefined
+
 const monacoSetup = (edit: editor.IStandaloneCodeEditor) => {
   watch(loadedPolicy, () => {
     edit.setScrollPosition({ scrollTop: 0 })
@@ -50,14 +52,34 @@ const monacoSetup = (edit: editor.IStandaloneCodeEditor) => {
 
   if (eventRegistered.value) return
 
-  edit.onKeyUp((e: any) => {
+  edit.onKeyUp((e) => {
     if (!autocompleteOnEnter.value) return
 
     const position = edit.getPosition()
     if (!position) return
+
     const text = edit.getModel()?.getLineContent(position.lineNumber).trim()
+
     if (e.keyCode === KeyCode.Enter && !text) {
-      edit.trigger('', 'editor.action.triggerSuggest', '')
+      if (typeof timeout === 'number') {
+        clearTimeout(timeout)
+      }
+
+      timeout = setTimeout(() => {
+        edit.trigger('', 'editor.action.triggerSuggest', '')
+      }, 1500)
+    } else if (e.keyCode === KeyCode.Escape && typeof timeout === 'number') {
+      clearTimeout(timeout)
+      timeout = undefined
+    } else if (typeof timeout === 'number' && !text) {
+      clearTimeout(timeout)
+
+      timeout = setTimeout(() => {
+        edit.trigger('', 'editor.action.triggerSuggest', '')
+      }, 1500)
+    } else if (typeof timeout === 'number') {
+      clearTimeout(timeout)
+      timeout = undefined
     }
   })
 
