192bb4413b22ea73582391732b10c0795086bbd0
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-06-12 17:19:20 +0200

    split
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- backend/pkg/engine/models/flags.go
+++ backend/pkg/engine/models/flags.go
@@ -1,19 +1,5 @@
 package models
 
-type Registry struct {
-	AllowInsecure     bool     `json:"allowInsecure"`
-	PullSecrets       []string `json:"pullSecrets"`
-	CredentialHelpers []string `json:"credentialHelpers"`
-}
-
-type ProtectManagedResources struct {
-	Enabled bool `json:"enabled"`
-}
-
-type ForceFailurePolicyIgnore struct {
-	Enabled bool `json:"enabled"`
-}
-
 type Flags struct {
 	Exceptions               Exceptions               `json:"exceptions"`
 	Cosign                   Cosign                   `json:"cosign"`
