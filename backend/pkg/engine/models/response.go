ede2a4ee88bf4140a5a5108d23c63a630182de3e
Charles-Edouard Brétéché <charles.edouard@nirmata.com>
2023-09-21 15:51:52 +0200

    fix: store policiy and vap in different response fields
    Signed-off-by: Charles-Edouard Brétéché <charles.edouard@nirmata.com>

--- backend/pkg/engine/models/response.go
+++ backend/pkg/engine/models/response.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	kyvernov1 "github.com/kyverno/kyverno/api/kyverno/v1"
+	"k8s.io/api/admissionregistration/v1alpha1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 )
 
@@ -12,6 +13,8 @@ type Response struct {
 	Resource unstructured.Unstructured `json:"resource"`
 	// Policy is the original policy
 	Policy kyvernov1.PolicyInterface `json:"policy"`
+	// ValidatingAdmissionPolicy is the original policy
+	ValidatingAdmissionPolicy *v1alpha1.ValidatingAdmissionPolicy `json:"validatingAdmissionPolicy"`
 	// namespaceLabels given by policy context
 	NamespaceLabels map[string]string `json:"namespaceLabels"`
 	// PatchedResource is the resource patched with the engine action changes
