e2d019d8e548a7919a6e29e6c57096d328da79f5
Frank Jogeleit <frank.jogeleit@web.de>
2023-10-19 18:45:04 +0200

    Update Dependencies (#536)
    Signed-off-by: Frank Jogeleit <frank.jogeleit@lovoo.com>

--- backend/pkg/cluster/policy_exception.go
+++ backend/pkg/cluster/policy_exception.go
@@ -3,7 +3,7 @@ package cluster
 import (
 	"context"
 
-	kyvernov2alpha1 "github.com/kyverno/kyverno/api/kyverno/v2alpha1"
+	"github.com/kyverno/kyverno/api/kyverno/v2beta1"
 	"github.com/kyverno/kyverno/pkg/client/clientset/versioned"
 	engineapi "github.com/kyverno/kyverno/pkg/engine/api"
 	kerrors "k8s.io/apimachinery/pkg/api/errors"
@@ -12,20 +12,20 @@ import (
 )
 
 type policyExceptionSelector struct {
-	additional    []*kyvernov2alpha1.PolicyException
+	additional    []*v2beta1.PolicyException
 	kyvernoClient versioned.Interface
 	namespace     string
 }
 
-func (c policyExceptionSelector) List(selector labels.Selector) ([]*kyvernov2alpha1.PolicyException, error) {
-	var exceptions []*kyvernov2alpha1.PolicyException
+func (c policyExceptionSelector) List(selector labels.Selector) ([]*v2beta1.PolicyException, error) {
+	var exceptions []*v2beta1.PolicyException
 	if c.kyvernoClient != nil {
 		list, err := c.kyvernoClient.KyvernoV2alpha1().PolicyExceptions(c.namespace).List(context.TODO(), metav1.ListOptions{
 			LabelSelector: selector.String(),
 		})
 		if err == nil {
 			for i := range list.Items {
-				pe := kyvernov2alpha1.PolicyException(list.Items[i])
+				pe := v2beta1.PolicyException(list.Items[i])
 				exceptions = append(exceptions, &pe)
 			}
 		} else if !kerrors.IsNotFound(err) {
@@ -40,7 +40,7 @@ func (c policyExceptionSelector) List(selector labels.Selector) ([]*kyvernov2alp
 	return exceptions, nil
 }
 
-func NewPolicyExceptionSelector(namespace string, client versioned.Interface, exceptions ...*kyvernov2alpha1.PolicyException) engineapi.PolicyExceptionSelector {
+func NewPolicyExceptionSelector(namespace string, client versioned.Interface, exceptions ...*v2beta1.PolicyException) engineapi.PolicyExceptionSelector {
 	return policyExceptionSelector{
 		additional:    exceptions,
 		kyvernoClient: client,
