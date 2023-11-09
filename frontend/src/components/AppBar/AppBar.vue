0a79d84b5a73eadadcaeba1aeb6b7fa308f399ed
Frank Jogeleit <frank.jogeleit@web.de>
2023-11-09 14:46:04 +0100

    update version info (#559)
    Signed-off-by: Frank Jogeleit <frank.jogeleit@lovoo.com>

--- frontend/src/components/AppBar/AppBar.vue
+++ frontend/src/components/AppBar/AppBar.vue
@@ -6,11 +6,11 @@
     <div class="app-logo">
       <template v-if="display.mdAndUp.value">
         <v-img src="/kyverno-logo.png" />
-        <v-chip size="small" style="position: absolute; bottom: 12px; right: -80px">v1.11-beta</v-chip>
+        <v-chip size="small" style="position: absolute; bottom: 12px; right: -80px">v1.11-rc</v-chip>
       </template>
       <template v-if="display.smAndDown.value">
         <v-img src="/favicon.png" width="80" />
-        <v-chip size="small" style="position: absolute; bottom: 16px; left: 80px">Kyverno v1.11-beta</v-chip>
+        <v-chip size="small" style="position: absolute; bottom: 16px; left: 80px">Kyverno v1.11-rc</v-chip>
       </template>
     </div>
     <template v-slot:append>
