91cd85ecc80eb29ec5f7dcd06c0e67e37dcd5f0c
Frank Jogeleit <frank.jogeleit@web.de>
2026-07-08 18:17:02 +0200

    feat(ui): click on details btn to copy err msg to clipboard (#1297)
    Signed-off-by: Frank Jogeleit <frank@giantswarm.io>

--- frontend/src/components/Results/MsgTooltip.vue
+++ frontend/src/components/Results/MsgTooltip.vue
@@ -1,15 +1,19 @@
 <template>
   <v-tooltip :text="msg" content-class="no-opacity-tooltip">
     <template v-slot:activator="{ props }">
-      <v-btn v-bind="props" icon="mdi-alert-circle-outline" variant="text" small />
+      <v-btn v-bind="props" @click="copy(msg)" :icon="copied ? 'mdi-alert-circle-check-outline' : 'mdi-alert-circle-outline'" variant="text" small />
     </template>
   </v-tooltip>
 </template>
 
 <script setup lang="ts">
-defineProps({
+import { useClipboard } from '@vueuse/core';
+
+const props = defineProps({
   msg: { type: String, required: true },
 })
+
+const { copy, copied } = useClipboard({ source: props.msg })
 </script>
 
 <style>
